3942c32d89c05535314d12255a34f861cc5a54b8
NathanWelsh <95314170+NathanWelsh@users.noreply.github.com>
2022-04-16 00:29:26 -0400

    Add files via upload

---
+++ WindowsFormAverageGUI/GreenvilleRevenueGUI/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace GreenvilleRevenueGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DisplayAverage = new System.Windows.Forms.Button();
+            this.DisplaySum = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.DisplayHigh = new System.Windows.Forms.Button();
+            this.DisplayLow = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter First Number >>";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Second Number >>";
+            // 
+            // DisplayAverage
+            // 
+            this.DisplayAverage.Location = new System.Drawing.Point(29, 157);
+            this.DisplayAverage.Name = "DisplayAverage";
+            this.DisplayAverage.Size = new System.Drawing.Size(100, 25);
+            this.DisplayAverage.TabIndex = 8;
+            this.DisplayAverage.Text = "Display Average";
+            this.DisplayAverage.UseVisualStyleBackColor = true;
+            this.DisplayAverage.Click += new System.EventHandler(this.DisplayAverage_Click);
+            // 
+            // DisplaySum
+            // 
+            this.DisplaySum.Location = new System.Drawing.Point(31, 189);
+            this.DisplaySum.Name = "DisplaySum";
+            this.DisplaySum.Size = new System.Drawing.Size(100, 25);
+            this.DisplaySum.TabIndex = 9;
+            this.DisplaySum.Text = "Sum";
+            this.DisplaySum.UseVisualStyleBackColor = true;
+            this.DisplaySum.Click += new System.EventHandler(this.DisplaySum_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LemonChiffon;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(147, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "The Average is";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LemonChiffon;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(149, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "The Sum is";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Enter Third Number >>";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 103);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Enter Fourth Number >>";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(36, 20);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(161, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(36, 20);
+            this.textBox3.TabIndex = 21;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(161, 100);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(36, 20);
+            this.textBox4.TabIndex = 22;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Enter Fifth Number >>";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(161, 127);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(36, 20);
+            this.textBox5.TabIndex = 24;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // DisplayHigh
+            // 
+            this.DisplayHigh.Location = new System.Drawing.Point(29, 220);
+            this.DisplayHigh.Name = "DisplayHigh";
+            this.DisplayHigh.Size = new System.Drawing.Size(100, 25);
+            this.DisplayHigh.TabIndex = 25;
+            this.DisplayHigh.Text = "High";
+            this.DisplayHigh.UseVisualStyleBackColor = true;
+            this.DisplayHigh.Click += new System.EventHandler(this.DisplayHigh_Click);
+            // 
+            // DisplayLow
+            // 
+            this.DisplayLow.Location = new System.Drawing.Point(29, 251);
+            this.DisplayLow.Name = "DisplayLow";
+            this.DisplayLow.Size = new System.Drawing.Size(100, 25);
+            this.DisplayLow.TabIndex = 26;
+            this.DisplayLow.Text = "Low";
+            this.DisplayLow.UseVisualStyleBackColor = true;
+            this.DisplayLow.Click += new System.EventHandler(this.DisplayLow_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.LemonChiffon;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(147, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 15);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "The Highest Number is";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.LemonChiffon;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label9.Location = new System.Drawing.Point(149, 251);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 15);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "The Lowest Number is";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(373, 351);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DisplayLow);
+            this.Controls.Add(this.DisplayHigh);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DisplaySum);
+            this.Controls.Add(this.DisplayAverage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Nathan Welsh\'s Form Assignment 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button DisplayAverage;
+        private System.Windows.Forms.Button DisplaySum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button DisplayHigh;
+        private System.Windows.Forms.Button DisplayLow;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ WindowsFormAverageGUI/GreenvilleRevenueGUI/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenvilleRevenueGUI
+{
+    //----------------------------------------
+    // Written By Dr. Roger Webster
+    // Written By Nathan Welsh
+    // COP2360 C# Programming I
+    // www.fsw.edu
+    // FALL 2020
+    //----------------------------------------
+    public partial class Form1 : Form
+    {
+        const int maxint = (int.MaxValue);
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        int num5;
+        int high = -maxint;
+        int low = maxint;
+        int sum = 0;
+        double average = 0;
+      
+        public Form1()
+        {
+            InitializeComponent();
+            
+            textBox1.Text = "1";
+            textBox2.Text = "2";
+            textBox3.Text = "3";
+            textBox4.Text = "4";
+            textBox5.Text = "5";
+
+
+            getNewData();
+        }
+
+        private void clearform()
+        {
+            label3.Text = "The Average here";
+            label4.Text = "The Sum here";
+            label6.Text = "The Highest Number here";
+            label9.Text = "The Lowest Number here";
+
+
+        }
+        private void getNewData()
+        {
+            num1 = Convert.ToInt32(textBox1.Text);
+            num2 = Convert.ToInt32(textBox2.Text);
+            num3 = Convert.ToInt32(textBox3.Text);
+            num4 = Convert.ToInt32(textBox4.Text);
+            num5 = Convert.ToInt32(textBox5.Text);
+            sum = num1 + num2 + num3 + num4 + num5;
+            average = (sum) / 5.0;
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            clearform();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            clearform();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            clearform();
+        }
+
+        private void DisplayHigh_Click(object sender, EventArgs e)
+        {
+            getNewData();
+
+            high = num1;
+            if (num2 > high)
+                high = num2;
+            if (num3 > high)
+                high = num3;
+            if (num4 > high)
+                high = num4;
+            if (num5 > high)
+                high = num5;
+            label6.Text = String.Format("The Highest Number is {0}", high);
+        }
+
+        private void DisplayLow_Click(object sender, EventArgs e)
+        {
+            getNewData();
+
+            low = num1;
+            if (num2 < low)
+                low = num2;
+            if (num3 < low)
+                low = num3;
+            if (num4 < low)
+                low = num4;
+            if (num5 < low)
+                low = num5;
+            label9.Text = String.Format("The Lowest Number is {0}", low);
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            clearform();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            clearform();
+        }
+
+        private void DisplayAverage_Click(object sender, EventArgs e)
+        {
+            getNewData();
+            label3.Text = String.Format("The Average is {0}", average);
+        }
+
+        private void DisplaySum_Click(object sender, EventArgs e)
+        {
+            getNewData();
+            label4.Text = String.Format("The Sum is {0}", sum);
+        }
+    }
+}
