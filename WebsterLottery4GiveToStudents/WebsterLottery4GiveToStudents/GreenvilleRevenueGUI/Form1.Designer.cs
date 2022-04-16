5fb5f7c1599ea343cf87e21c743e8b8873aac846
NathanWelsh <95314170+NathanWelsh@users.noreply.github.com>
2022-04-16 00:27:32 -0400

    Add files via upload

---
+++ WebsterLottery4GiveToStudents/WebsterLottery4GiveToStudents/GreenvilleRevenueGUI/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace GreenvilleRevenueGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputLabel1 = new System.Windows.Forms.Label();
+            this.outputLabel2 = new System.Windows.Forms.Label();
+            this.outputLabel3 = new System.Windows.Forms.Label();
+            this.outputLabel4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.outputlabel5 = new System.Windows.Forms.Label();
+            this.outputlabel6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your Numbers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(295, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Florida Lotto Numbers";
+            // 
+            // outputLabel1
+            // 
+            this.outputLabel1.AutoSize = true;
+            this.outputLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel1.Location = new System.Drawing.Point(283, 328);
+            this.outputLabel1.Name = "outputLabel1";
+            this.outputLabel1.Size = new System.Drawing.Size(18, 20);
+            this.outputLabel1.TabIndex = 4;
+            this.outputLabel1.Text = "0";
+            // 
+            // outputLabel2
+            // 
+            this.outputLabel2.AutoSize = true;
+            this.outputLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel2.Location = new System.Drawing.Point(327, 328);
+            this.outputLabel2.Name = "outputLabel2";
+            this.outputLabel2.Size = new System.Drawing.Size(18, 20);
+            this.outputLabel2.TabIndex = 5;
+            this.outputLabel2.Text = "0";
+            // 
+            // outputLabel3
+            // 
+            this.outputLabel3.AutoSize = true;
+            this.outputLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel3.Location = new System.Drawing.Point(378, 328);
+            this.outputLabel3.Name = "outputLabel3";
+            this.outputLabel3.Size = new System.Drawing.Size(18, 20);
+            this.outputLabel3.TabIndex = 6;
+            this.outputLabel3.Text = "0";
+            // 
+            // outputLabel4
+            // 
+            this.outputLabel4.AutoSize = true;
+            this.outputLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel4.Location = new System.Drawing.Point(425, 328);
+            this.outputLabel4.Name = "outputLabel4";
+            this.outputLabel4.Size = new System.Drawing.Size(18, 20);
+            this.outputLabel4.TabIndex = 7;
+            this.outputLabel4.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(9, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 192);
+            this.button1.TabIndex = 8;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(2, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(561, 204);
+            this.button2.TabIndex = 9;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 272);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(322, 272);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(373, 272);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(36, 20);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(428, 272);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(36, 20);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(261, 350);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(272, 26);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "The Number of Matches is ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(279, 391);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 28);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "You get ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(478, 272);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(36, 20);
+            this.textBox5.TabIndex = 18;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(521, 272);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(36, 20);
+            this.textBox6.TabIndex = 19;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // outputlabel5
+            // 
+            this.outputlabel5.AutoSize = true;
+            this.outputlabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputlabel5.Location = new System.Drawing.Point(463, 328);
+            this.outputlabel5.Name = "outputlabel5";
+            this.outputlabel5.Size = new System.Drawing.Size(18, 20);
+            this.outputlabel5.TabIndex = 20;
+            this.outputlabel5.Text = "0";
+            // 
+            // outputlabel6
+            // 
+            this.outputlabel6.AutoSize = true;
+            this.outputlabel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputlabel6.Location = new System.Drawing.Point(495, 328);
+            this.outputlabel6.Name = "outputlabel6";
+            this.outputlabel6.Size = new System.Drawing.Size(18, 20);
+            this.outputlabel6.TabIndex = 21;
+            this.outputlabel6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 682);
+            this.Controls.Add(this.outputlabel6);
+            this.Controls.Add(this.outputlabel5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputLabel4);
+            this.Controls.Add(this.outputLabel3);
+            this.Controls.Add(this.outputLabel2);
+            this.Controls.Add(this.outputLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Florida Lotto!!!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label outputLabel1;
+        private System.Windows.Forms.Label outputLabel2;
+        private System.Windows.Forms.Label outputLabel3;
+        private System.Windows.Forms.Label outputLabel4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label outputlabel5;
+        private System.Windows.Forms.Label outputlabel6;
+    }
+}
+
---
+++ WebsterLottery4GiveToStudents/WebsterLottery4GiveToStudents/GreenvilleRevenueGUI/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//----------------------------------------
+// Written By Nathan Welsh
+// COP2360 C# Programming I
+// www.fsw.edu
+// Dr. Roger Webster - Professor
+//----------------------------------------
+namespace GreenvilleRevenueGUI
+{
+    public partial class Form1 : Form
+    {
+        Random ranNumberGenerator;
+
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int num4 = 0;
+        int num5 = 0;
+        int num6 = 0;
+
+        int lottonum1 = 0;
+        int lottonum2 = 0;
+        int lottonum3 = 0;
+        int lottonum4 = 0;
+        int lottonum5 = 0;
+        int lottonum6 = 0;
+
+        int totalwinnings = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //preset the numbers!!!
+            textBox1.Text = "1";
+            textBox2.Text = "2";
+            textBox3.Text = "3";
+            textBox4.Text = "4";
+            textBox5.Text = "5";
+            textBox6.Text = "6";
+            ranNumberGenerator = new Random();
+            label6.Text = "";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //this is the main button
+
+
+            getNewData();
+
+            setUpLotteryNumbers();
+
+            check_forDuplicates();
+
+            int nummatches = check_for_Winnings();
+
+            NumberOfMatches();
+
+            NumberOfWinnings();
+
+        }
+
+        private void setUpLotteryNumbers()
+        {
+
+            lottonum1 = check_forDuplicates();
+            outputLabel1.Text = Convert.ToString(lottonum1);
+
+            lottonum2 = check_forDuplicates();
+            outputLabel2.Text = Convert.ToString(lottonum2);
+
+            lottonum3 = check_forDuplicates();
+            outputLabel3.Text = Convert.ToString(lottonum3);
+
+            lottonum4 = check_forDuplicates();
+            outputLabel4.Text = Convert.ToString(lottonum4);
+
+            lottonum5 = check_forDuplicates();
+            outputlabel5.Text = Convert.ToString(lottonum5);
+
+            lottonum6 = check_forDuplicates();
+            outputlabel6.Text = Convert.ToString(lottonum6);
+
+
+        }
+
+
+        private void getNewData()
+        {
+            num1 = Convert.ToInt32(textBox1.Text);
+            num2 = Convert.ToInt32(textBox2.Text);
+            num3 = Convert.ToInt32(textBox3.Text);
+            num4 = Convert.ToInt32(textBox4.Text);
+            num5 = Convert.ToInt32(textBox5.Text);
+            num6 = Convert.ToInt32(textBox6.Text);
+
+
+        }
+
+        private int check_forDuplicates()
+        {
+            int randomNumber = 0;
+
+            Boolean duplicates = true;
+
+            while (duplicates)
+            {
+                duplicates = false;
+                randomNumber = ranNumberGenerator.Next(1, 54);
+                if (randomNumber == lottonum1)
+                    duplicates = true;
+                if (randomNumber == lottonum2)
+                    duplicates = true;
+                if (randomNumber == lottonum3)
+                    duplicates = true;
+                if (randomNumber == lottonum4)
+                    duplicates = true;
+                if (randomNumber == lottonum5)
+                    duplicates = true;
+                if (randomNumber == lottonum6)
+                    duplicates = true;
+            }
+            return randomNumber;
+        }
+
+        private int check_for_Winnings()
+        {
+            int nummatches = 0;
+
+            if (lottonum1 == num1 || lottonum1 == num2 || lottonum1 == num3 || lottonum1 == num4 || lottonum1 == num5 || lottonum1 == num6)
+                nummatches++;
+            if (lottonum2 == num1 || lottonum2 == num2 || lottonum2 == num3 || lottonum2 == num4 || lottonum2 == num5 || lottonum2 == num6)
+                nummatches++;
+            if (lottonum3 == num1 || lottonum3 == num2 || lottonum3 == num3 || lottonum3 == num4 || lottonum3 == num5 || lottonum3 == num6)
+                nummatches++;
+            if (lottonum4 == num1 || lottonum4 == num2 || lottonum4 == num3 || lottonum4 == num4 || lottonum4 == num5 || lottonum4 == num6)
+                nummatches++;
+            if (lottonum5 == num1 || lottonum5 == num2 || lottonum5 == num3 || lottonum5 == num4 || lottonum5 == num5 || lottonum5 == num6)
+                nummatches++;
+            if (lottonum6 == num1 || lottonum6 == num2 || lottonum6 == num3 || lottonum6 == num4 || lottonum6 == num5 || lottonum6 == num6)
+                nummatches++;
+
+
+
+
+            return nummatches;
+        }
+
+        private void NumberOfMatches()
+        {
+            int nummatches = check_for_Winnings();
+
+
+            if (nummatches == 0)
+                label5.Text = ("The Number of Matches is 0");
+
+            if (nummatches == 1)
+                label5.Text = ("The Number of Matches is 1");
+
+            if (nummatches == 2)
+                label5.Text = ("The Number of Matches is 2");
+
+            if (nummatches == 3)
+                label5.Text = ("The Number of Matches is 3");
+
+            if (nummatches == 4)
+                label5.Text = ("The Number of Matches is 4");
+
+            if (nummatches == 5)
+                label5.Text = ("The Number of Matches is 5");
+
+            if (nummatches == 6)
+                label5.Text = ("The Number of Matches is 6");
+
+        }
+
+        private void NumberOfWinnings()
+        {
+
+            int nummatches = check_for_Winnings();
+
+            if (nummatches == 0)
+                label6.Text = ("You Lost!");
+
+            if (nummatches == 1)
+                label6.Text = ("You Lost!");
+
+            if (nummatches == 2)
+                label6.Text = ("You Win a Free Ticket!");
+
+            if (nummatches == 3)
+                label6.Text = ("You Win $25!");
+
+            if (nummatches == 4)
+                label6.Text = ("You Win $300");
+
+            if (nummatches == 5)
+                label6.Text = ("You Win $13,000");
+
+            if (nummatches == 6)
+                label6.Text = ("You Win $5,000,000");
+        }
+    }
+}
+
