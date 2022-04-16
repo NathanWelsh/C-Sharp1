65edb64cc5a45bb924c9cd030b2b09dcaa558715
NathanWelsh <95314170+NathanWelsh@users.noreply.github.com>
2022-04-16 00:26:02 -0400

    Add files via upload

---
+++ COP2360QuizWebsterGiveToStudents/GreenvilleRevenueGUI/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace GreenvilleRevenueGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.msglabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.CapitalAnswertextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numcorrectlabel = new System.Windows.Forms.Label();
+            this.Instructionslabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "The Capital is: ";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(9, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 55);
+            this.button1.TabIndex = 8;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(145, 83);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(231, 26);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "What is the Capital of: ";
+            // 
+            // msglabel
+            // 
+            this.msglabel.AutoSize = true;
+            this.msglabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.msglabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.msglabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msglabel.Location = new System.Drawing.Point(149, 149);
+            this.msglabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.msglabel.Name = "msglabel";
+            this.msglabel.Size = new System.Drawing.Size(122, 22);
+            this.msglabel.TabIndex = 17;
+            this.msglabel.Text = "Found N Words";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 24);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Dr. Webster\'s COP2360 STATE CAPITAL GAME";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 149);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 29);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Cheat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // CapitalAnswertextBox
+            // 
+            this.CapitalAnswertextBox.Location = new System.Drawing.Point(361, 118);
+            this.CapitalAnswertextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.CapitalAnswertextBox.Name = "CapitalAnswertextBox";
+            this.CapitalAnswertextBox.Size = new System.Drawing.Size(177, 20);
+            this.CapitalAnswertextBox.TabIndex = 23;
+            this.CapitalAnswertextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(363, 83);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 26);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Massachusetts";
+            // 
+            // numcorrectlabel
+            // 
+            this.numcorrectlabel.AutoSize = true;
+            this.numcorrectlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numcorrectlabel.Location = new System.Drawing.Point(175, 45);
+            this.numcorrectlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.numcorrectlabel.Name = "numcorrectlabel";
+            this.numcorrectlabel.Size = new System.Drawing.Size(297, 18);
+            this.numcorrectlabel.TabIndex = 25;
+            this.numcorrectlabel.Text = "Number Correct: 0 Out of 0 Total Questions";
+            // 
+            // Instructionslabel
+            // 
+            this.Instructionslabel.AutoSize = true;
+            this.Instructionslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Instructionslabel.Location = new System.Drawing.Point(18, 192);
+            this.Instructionslabel.Name = "Instructionslabel";
+            this.Instructionslabel.Size = new System.Drawing.Size(72, 15);
+            this.Instructionslabel.TabIndex = 26;
+            this.Instructionslabel.Text = "Instructions ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 244);
+            this.Controls.Add(this.Instructionslabel);
+            this.Controls.Add(this.numcorrectlabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CapitalAnswertextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.msglabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "State Capital Game!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label msglabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox CapitalAnswertextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label numcorrectlabel;
+        private System.Windows.Forms.Label Instructionslabel;
+    }
+}
+
---
+++ COP2360QuizWebsterGiveToStudents/GreenvilleRevenueGUI/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Input;
+using static System.Console;
+//-------------------------------------------------
+// Written By Dr. Roger Webster
+// Modified By Nathan Welsh
+// For: COP 2360 C# Computer Programming
+// Where: FSW Computer Science Program www.fsw.edu
+// Professor: Dr. Roger Webster
+// When: FALL 2020
+//--------------------------------------------------
+namespace GreenvilleRevenueGUI
+{
+    public partial class Form1 : Form
+    {
+        String[] States = new String[50];
+        String[] Capitals = new String[50];
+        String answer = "";
+        int statenumber = 0;
+        Random ranNumberGenerator;
+        int TotalQs = 0;// use this to keep track of total qs
+        int numcorrect = 0;// use this to keep track of num correct
+
+        public Form1()
+        {
+            InitializeComponent();
+            msglabel.Text = "";
+            this.Size = new Size(600, 300);
+            ranNumberGenerator = new Random();
+            statenumber = ranNumberGenerator.Next(0, 49);
+            LoadStates();
+            label3.Text = States[statenumber];
+
+
+            Instructionslabel.Text = "Please type in your answer and press Enter key. Press Button gives you the next question.";
+        }
+
+        public void LoadStates()
+        {
+            States[0] = "Alabama";
+            Capitals[0] = "Montgomery";
+            States[1] = "Alaska";
+            Capitals[1] = "Juneau";
+            States[2] = "Arizona";
+            Capitals[2] = "Phoenix";
+            States[3] = "Arkansas";
+            Capitals[3] = "Little Rock";
+            States[4] = "California";
+            Capitals[4] = "Sacramento";
+            States[5] = "Colorado";
+            Capitals[5] = "Denver";
+            States[6] = "Connecticut";
+            Capitals[6] = "Hartford";
+            States[7] = "Delaware";
+            Capitals[7] = "Dover";
+            States[8] = "Florida";
+            Capitals[8] = "Tallahassee";
+            States[9] = "Georgia";
+            Capitals[9] = "Atlanta";
+            States[10] = "Hawaii";
+            Capitals[10] = "Honolulu";
+            States[11] = "Idaho";
+            Capitals[11] = "Boise";
+            States[12] = "Illinois";
+            Capitals[12] = "Springfield";
+            States[13] = "Indiana";
+            Capitals[13] = "Indianapolis";
+            States[14] = "Iowa";
+            Capitals[14] = "Des Moines";
+            States[15] = "Kansas";
+            Capitals[15] = "Topeka";
+            States[16] = "Kentucky";
+            Capitals[16] = "Frankfort";
+            States[17] = "Louisiana";
+            Capitals[17] = "Baton Rouge";
+            States[18] = "Maine";
+            Capitals[18] = "Augusta";
+            States[19] = "Maryland";
+            Capitals[19] = "Annapolis";
+            States[20] = "Massachusetts";
+            Capitals[20] = "Boston";
+            States[21] = "Michigan";
+            Capitals[21] = "Lansing";
+            States[22] = "Minnesota";
+            Capitals[22] = "St. Paul";
+            States[23] = "Mississippi";
+            Capitals[23] = "Jackson";
+            States[24] = "Missouri";
+            Capitals[24] = "Jefferson City";
+            States[25] = "Montana";
+            Capitals[25] = "Helena";
+            States[26] = "Nebraska";
+            Capitals[26] = "Lincoln";
+            States[27] = "Nevada";
+            Capitals[27] = "Carson City";
+            States[28] = "New Hampshire";
+            Capitals[28] = "Concord";
+            States[29] = "New Jersey";
+            Capitals[29] = "Trenton";
+            States[30] = "New Mexico";
+            Capitals[30] = "Santa Fe";
+            States[31] = "New York";
+            Capitals[31] = "Albany";
+            States[32] = "North Carolina";
+            Capitals[32] = "Raleigh";
+            States[33] = "North Dakota";
+            Capitals[33] = "Bismarck";
+            States[34] = "Ohio";
+            Capitals[34] = "Columbus";
+            States[35] = "Oklahoma";
+            Capitals[35] = "Oklahoma City";
+            States[36] = "Oregon";
+            Capitals[36] = "Salem";
+            States[37] = "Pennsylvania";
+            Capitals[37] = "Harrisburg";
+            States[38] = "Rhode Island";
+            Capitals[38] = "Providence";
+            States[39] = "South Carolina";
+            Capitals[39] = "Columbia";
+            States[40] = "South Dakota";
+            Capitals[40] = "Pierre";
+            States[41] = "Tennessee";
+            Capitals[41] = "Nashville";
+            States[42] = "Texas";
+            Capitals[42] = "Austin";
+            States[43] = "Utah";
+            Capitals[43] = "Salt Lake City";
+            States[44] = "Vermont";
+            Capitals[44] = "Montpelier";
+            States[45] = "Virginia";
+            Capitals[45] = "Richmond";
+            States[46] = "Washington";
+            Capitals[46] = "Olympia";
+            States[47] = "West Virginia";
+            Capitals[47] = "Charleston";
+            States[48] = "Wisconsin";
+            Capitals[48] = "Madison";
+            States[49] = "Wyoming";
+            Capitals[49] = "Cheyenne";
+        }
+        //----------------------------------------------
+        // this is the get next Q button
+        //----------------------------------------------
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            statenumber = ranNumberGenerator.Next(0, 49);
+            label3.Text = States[statenumber];
+            TotalQs += 1;
+            msglabel.Text = "";
+        }
+
+        //--------------------------------------------------------------------
+        // this is the cheat button, please put correct answer in the label
+        //--------------------------------------------------------------------
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            CapitalAnswertextBox.Text = ("") + Capitals[statenumber];
+
+        }
+        //-----------------------------------------------------------------------
+        // this is the text event handler, when the user presses enter this runs
+        //-----------------------------------------------------------------------
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                //here is where you put your code
+                //MessageBox.Show("The answer is: " + CapitalAnswertextBox.Text);
+                answer = CapitalAnswertextBox.Text;
+
+                if (CapitalAnswertextBox.Text.ToLower() == Capitals[statenumber].ToLower())
+                {
+                    numcorrect++;
+                    TotalQs++;
+                    msglabel.Text = ("You are Correct! ") + Capitals[statenumber];
+                }
+                else
+                {
+                    msglabel.Text = ("You are not Correct! ") + Capitals[statenumber] + " Is the capital of " + States[statenumber];
+                }
+                setScore();
+                CapitalAnswertextBox.Text = "";
+            }
+        }
+        public void setScore()
+        {
+            numcorrectlabel.Text = ("Number Correct: ") + numcorrect.ToString() + (" Out of ") + TotalQs.ToString() + " Total Questions";
+
+        }
+    }
+}
+
